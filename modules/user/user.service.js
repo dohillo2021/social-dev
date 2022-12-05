8cf085fac2967c30366bb910994de79edebbbe98
William Machado <dohillo@gmail.com>
2022-12-04 23:29:17 -0300

    Criado sistema de Login

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,4 +1,4 @@
-import { hashPassword } from '../../utils/bcrypt'
+import { hashPassword, comparePassword } from '../../utils/bcrypt'
 
 import User from './user.model'
 
@@ -12,8 +12,24 @@ export const signupUser = async (body) => {
     return dbUser 
   } catch (err) {
     throw err
-  }
-      
+  }      
 }
-
   
+export const login = async (body) => {
+  try {
+    const user = await User.findOne({
+      $or: [
+        { email: body.userOrEmail },
+        { user: body.userOrEmail}
+      ]
+    })
+
+    if (!user) throw new Error('not found')
+    const passwordIsCorrect = comparePassword(body.password, user.password)
+    if (!passwordIsCorrect) throw new Error('password incorrect')
+
+    return user
+  } catch (err) {
+    throw err
+  }
+}
