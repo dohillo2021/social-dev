f883b7facaddd109c373a7382a2bad2bafc5e835
William Machado <dohillo@gmail.com>
2022-12-05 23:10:13 -0300

    Criado useState para envio de formulário

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import styled from 'styled-components'
 import Link from 'next/link'
 
@@ -24,19 +26,37 @@ const Text = styled.p`
 `
 
 function SignupPage ( ) {
+  const [firstName,setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [user, setUser] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+const handleForm = (event) => {
+  event.preventDefault()
+  console.log({
+    firstName,
+    lastName,
+    user,
+    email,
+    password
+  })
+}
+
+
   return (
     <ImageWithSpace>
       <H1># Social Dev</H1>
       <H4>Tudo que acontece no mundo dev, está aqui!</H4>
       <FormContainer>
         <H2>Crie sua conta</H2>
-        <Form>
-          <Input Label="Nome"/>
-          <Input Label="Sobrenome"/>
-          <Input Label="Usuário"/>
-          <Input Label="Email ou usuário" type="email" />
-          <Input Label="Senha" type="password" />
-          <Button>Entrar</Button>
+        <Form onSubmit={handleForm}>
+          <Input Label="Nome" onChange={({ target }) => {setFirstName(target.value)}}  />
+          <Input Label="Sobrenome" onChange={({ target }) => setLastName(target.value)} />
+          <Input Label="Usuário" onChange={({ target }) => setUser(target.value)}/>
+          <Input Label="Email" type="email" onChange={({ target }) => setEmail(target.value)} />
+          <Input Label="Senha" type="password" onChange={({ target }) => setPassword(target.value)}/>
+          <Button>Cadastrar</Button>
         </Form>
         <text>já possui uma conta? <Link href="/login">Faça seu login</Link></text>
       </FormContainer>
